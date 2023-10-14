dc2f210c0fa37c72c4882e11a85da50862a64b6e
Nikita Jha <nikitajha1912@gmail.com>
2023-10-14 14:22:10 -0400

    Added nav bar to App.tsx page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Button from './Components/Button'
 import ListingCard from "./Components/ListingCard"
+import NavBar from "./Components/NavBar"
 
 function App() {
 
   return (
     <>
+    <NavBar></NavBar>
     <h1>Hello World! Venusha is gay</h1>
     <Button></Button>
     <ListingCard></ListingCard>
