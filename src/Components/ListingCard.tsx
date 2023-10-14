6a2465154485db8e19d7e72a59dde1c3300555f9
Sahit Mamidipaka <smamidipaka6@gatech.edu>
2023-10-14 10:42:22 -0400

    updated listingcard w resizeable text card component

--- src/Components/ListingCard.tsx
+++ src/Components/ListingCard.tsx
@@ -4,30 +4,62 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { styled } from "@mui/material";
-import Avatar from "./Avatar"
+import Avatar from "./Avatar";
+import Grid from "@mui/material/Grid";
+import TurnedInNotIcon from "@mui/icons-material/TurnedInNot";
+import CloseIcon from "@mui/icons-material/Close";
+import ResizeableTextCard from "./ResizeableTextCard";
 
 const StyledCard = styled(Card)({
   borderRadius: "8px", // TODO: Edit this
   backgroundColor: "#333333",
-  border: "1px solid #484747"
-})
+  border: "1px solid #484747",
+  width: "75%",
+  height: 360 // "100%"
+});
 
 const StyledTypography = styled(Typography)({
-  color: '#FFFFFF',
-  fontFamily: 'Helvetica Neue',
+  color: "#FFFFFF",
+  fontFamily: "Helvetica Neue",
   fontWeight: 400,
-})
+});
 
 const card = (
   <React.Fragment>
     <CardContent>
+      <Grid
+        container
+        spacing={2}
+        alignItems="top"
+        sx={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <Grid item xl={2}>
+          <TurnedInNotIcon sx={{ color: "#FFFFFF" }}></TurnedInNotIcon>
+        </Grid>
+        <Grid item xl={8}>
+          <Avatar></Avatar>
+        </Grid>
+        <Grid item xl={2}>
+          <CloseIcon sx={{ color: "#FFFFFF" }}></CloseIcon>
+        </Grid>
+      </Grid>
 
-      <Avatar></Avatar>
-      
       <StyledTypography sx={{ mb: 1.5 }} color="text.secondary">
-        Just a blank card... For now!
+        Sahit Mamidipaka
       </StyledTypography>
-      
+
+      <ResizeableTextCard
+        width={140}
+        height={80}
+        text1={"$1560"}
+        text2={"Total: $9420"}
+      ></ResizeableTextCard>
+      <ResizeableTextCard
+        width={250}
+        height={80}
+        text1={"The Mark"}
+        text2={"Midtown"}
+      ></ResizeableTextCard>
     </CardContent>
     {/* <CardActions>
       <Button size="small">Learn More</Button>
@@ -38,7 +70,7 @@ const card = (
 const ListingCard = () => {
   return (
     <Box sx={{ minWidth: 275 }}>
-      <StyledCard variant="outlined">{card}</StyledCard> 
+      <StyledCard variant="outlined">{card}</StyledCard>
     </Box>
   );
 };
