f00a25f9d024e06fe0007ce5f80e13604c0bf1e8
Sahit Mamidipaka <smamidipaka6@gatech.edu>
2023-10-14 20:49:23 -0400

    attribute tag added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Button from "./Components/Button";
 import GroupDesc from "./Components/ListingCard/GroupDesc";
 import ListingCard from "./Components/ListingCard/ListingCard";
+import AttributeTag from "./Components/ListingCard/AttributeTags/PeopleAttribute"
 
 function App() {
   return (
     <>
       <GroupDesc></GroupDesc>
+      <AttributeTag text={"Hello"}></AttributeTag>
       <h1>Hello World! Venusha is gay</h1>
       <Button></Button>
       <ListingCard></ListingCard>
