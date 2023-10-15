40bcb36a80f43aa60bd3a1f4cf47194b2ffb5d96
Sahit Mamidipaka <smamidipaka6@gatech.edu>
2023-10-14 20:49:04 -0400

    added icons

--- src/Components/ListingCard/ListingCard.tsx
+++ src/Components/ListingCard/ListingCard.tsx
@@ -9,6 +9,11 @@ import Grid from "@mui/material/Grid";
 import TurnedInNotIcon from "@mui/icons-material/TurnedInNot";
 import CloseIcon from "@mui/icons-material/Close";
 import ResizeableTextCard from "./ResizeableTextCard";
+import GroupDesc from "./GroupDesc";
+import SizeAttribute from "./AttributeTags/SizeAttribute";
+import PeopleAttribute from "./AttributeTags/PeopleAttribute";
+import MaleGenderAttribute from "./AttributeTags/MaleGenderAttribute";
+import { Male } from "@mui/icons-material";
 
 const StyledCard = styled(Card)({
   borderRadius: "8px", // TODO: Edit this
@@ -26,47 +31,105 @@ const StyledTypography = styled(Typography)({
 
 const card = (
   <React.Fragment>
-    <CardContent>
-      <Grid
-        container
-        spacing={2}
-        alignItems="top"
-        sx={{ display: "flex", justifyContent: "space-between" }}
-      >
-        <Grid item xl={2}>
-          <TurnedInNotIcon sx={{ color: "#FFFFFF" }}></TurnedInNotIcon>
+    <CardContent sx={{ margin: 0, padding: 0 }}>
+      <Box>
+        <Grid
+          container
+          spacing={0}
+          alignItems="top"
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Grid item xs={1} sx={{ paddingTop: "12px" }}>
+            <TurnedInNotIcon sx={{ color: "#FFFFFF" }}></TurnedInNotIcon>
+          </Grid>
+          <Grid item xs={10}>
+            <Avatar></Avatar>
+          </Grid>
+          <Grid item xs={1} sx={{ paddingTop: "12px" }}>
+            <CloseIcon sx={{ color: "#FFFFFF" }}></CloseIcon>
+          </Grid>
         </Grid>
-        <Grid item xl={8}>
-          <Avatar></Avatar>
-        </Grid>
-        <Grid item xl={2}>
-          <CloseIcon sx={{ color: "#FFFFFF" }}></CloseIcon>
-        </Grid>
-      </Grid>
+      </Box>
 
       <StyledTypography sx={{ mb: 1.5 }} color="text.secondary">
         Sahit Mamidipaka
       </StyledTypography>
 
-      <Grid container spacing={2} sx={{ justifyContent: "center" }}>
-        <Grid item xl={4}>
-          <ResizeableTextCard
-            width={180}
-            height={80}
-            text1={"$1560"}
-            text2={"Total: $9360"}
-          ></ResizeableTextCard>
+      <Box sx={{ width: "100%", border: "" }}>
+        <Grid
+          container
+          spacing={0}
+          sx={{ alignContent: "center", padding: "0px", magin: "0" }}
+        >
+          <Grid
+            item
+            xs={5.5}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "0px",
+            }}
+          >
+            <ResizeableTextCard
+              width={180}
+              height={80}
+              text1={"$1560"}
+              text2={"Total: $9360"}
+            ></ResizeableTextCard>
+          </Grid>
+          <Grid
+            item
+            xs={6.5}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "0px",
+            }}
+          >
+            <ResizeableTextCard
+              width={250}
+              height={80}
+              text1={"The Mark"}
+              text2={"Midtown"}
+            ></ResizeableTextCard>
+          </Grid>
         </Grid>
-        <Grid item xl={8}>
-          <ResizeableTextCard
-            width={250}
-            height={80}
-            text1={"The Mark"}
-            text2={"Midtown"}
-          ></ResizeableTextCard>
+      </Box>
+
+      <Box sx={{ paddingTop: "8px" }}>
+        <Grid container spacing={0} sx={{ border: "" }}>
+          <Grid
+            item
+            xs={5}
+            sx={{
+              paddingLeft: "28px",
+              paddingRight: "10px",
+              border: "",
+            }}
+          >
+            <Grid
+              container
+              rowSpacing={1.5}
+              sx={{ border: "", justifyContent: "space-evenly" }}
+            >
+              <Grid item>
+                <PeopleAttribute text="4"></PeopleAttribute>
+              </Grid>
+              <Grid item>
+                <SizeAttribute text="4x4"></SizeAttribute>
+              </Grid>
+              <Grid item>
+                <MaleGenderAttribute text="Male"></MaleGenderAttribute>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={5} sx={{ border: "" }}>
+            <GroupDesc></GroupDesc>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </CardContent>
+
     {/* <CardActions>
       <Button size="small">Learn More</Button>
     </CardActions> */}
@@ -75,8 +138,10 @@ const card = (
 
 const ListingCard = () => {
   return (
-    <Box sx={{ minWidth: 275 }}>
-      <StyledCard variant="outlined">{card}</StyledCard>
+    <Box sx={{ minWidth: 0 }}>
+      <StyledCard variant="outlined" sx={{}}>
+        {card}
+      </StyledCard>
     </Box>
   );
 };
